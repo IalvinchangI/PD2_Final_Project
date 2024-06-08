6eb00ef355c309be7f36ddc8737b6a61d408b9eb
IalvinchangI <99197625+IalvinchangI@users.noreply.github.com>
2024-06-08 19:35:47 +0800

    create file

---
+++ project/App.java
@@ -0,0 +1,14 @@
+package project;
+
+/**
+ * main class for this project
+ */
+public class App {
+    /**
+     * 程式進入點
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+}
