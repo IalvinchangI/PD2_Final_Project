fafd34f069fd03b37fae591cae81342996bcfa22
hanklo831 <hanklo831@gmail.com>
2024-06-14 02:26:41 +0800

    新增history, stockdetail

---
+++ project/GUI/GUITools/StockDetail.java
@@ -0,0 +1,66 @@
+package GUITools;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.util.*;
+import GUITools.InputField;
+
+/**
+ * 股票的顯示細節
+ * @author Hanklo
+ */
+public class StockDetail extends JPanel {
+
+    protected JPanel stock;
+    protected JLabel stockNameLabel, stockPriceLabel;
+
+    protected FlowLayout stockLayout;
+
+    protected JPanel buyPanel, sellPanel, intervalPanel;
+
+
+
+    /**
+     * 建造：左上股票名、右上股票價格、中間K線圖、下面買賣處理
+     * @param stockName ：股票名稱
+     * @param stockPrice ：股票價格
+     * @param stockHistoryPrice ：股票近三十天的最高價、最低價、開盤價、收盤價
+     */
+    protected StockDetail(String stockName, double stockPrice, HashMap <String, ArrayList <Double>> stockHistoryPrice) {
+
+        // layout
+        this.stockLayout = new FlowLayout();
+
+        // panel
+        this.stock = new JPanel();
+
+        // label
+        this.stockNameLabel = new JLabel(stockName);
+        this.stockPriceLabel = new JLabel(Double.toString(stockPrice));
+
+        this.stock.add(this.stockNameLabel);
+        this.stock.add(this.stockPriceLabel);
+        
+        // graph
+
+
+
+        //inputField    
+        this.buyPanel = new InputField("buy");
+        this.sellPanel = new InputField("sell");
+        this.intervalPanel = new InputField("interval");
+
+        this.setLayout(stockLayout);
+        this.add(this.stock);
+        this.add(this.buyPanel);
+        this.add(this.sellPanel);
+        this.add(this.intervalPanel);
+
+
+    }
+
+
+}
---
+++ project/GUI/HistoryPanel.java
@@ -0,0 +1,54 @@
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+/**
+ * 歷史紀錄的顯示
+ * @author Hanklo
+ */
+public class HistoryPanel  extends JPanel {
+    
+}
+
+/**
+ *  Panel of profit and loss
+ */
+class ProfitPanel extends JPanel {
+
+    private JPanel profitPanel;
+    private double profit = 10.01;
+    private JLabel profitLabel;
+
+    ProfitPanel () {
+        profitPanel = new JPanel();
+        profitLabel = new JLabel(Double.toString(profit));
+        profitPanel.add(profitLabel);
+    }
+    
+}
+
+
+/**
+ * Panel of history trade record
+ */
+class HistoryTradePanel extends JPanel {
+
+    private JPanel historyTradePanel;
+    private JLabel historyTradeLabel;
+    private JScrollPane historyTradeScrollPane;
+    private String[] tradeRecord = {"aaa 100 2023", "aaa 100 2023", "aaa 100 2023", 
+    "aaa 100 2023", "aaa 100 2023", "aaa 100 2023", "aaa 100 2023", "aaa 100 2023", 
+    "aaa 100 2023", "aaa 100 2023", "aaa 100 2023", "aaa 100 2023"};
+
+    HistoryTradePanel () {
+        StringBuilder html = new StringBuilder("<html>");
+        for (String str : tradeRecord) {
+            html.append(str).append("<br>");
+        }
+        html.append("</html>");
+        historyTradeLabel = new JLabel(html.toString());
+        historyTradeScrollPane = new JScrollPane(historyTradeLabel);
+        historyTradePanel.add(historyTradeScrollPane);
+
+    }   
+}
