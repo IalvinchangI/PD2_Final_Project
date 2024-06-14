fca574e6bffe09cee6e0b5b35da94680296e96cb
Mason <f74121092@gs.ncku.edu.tw>
2024-06-14 11:08:00 +0800

    將除了StockDataSystem以外的interfaces改成class

--- project/System/Deal.java
+++ project/System/Deal.java
@@ -2,36 +2,51 @@ package project.System;
 
 import java.util.Date;;
 
-public interface Deal {
+public class Deal {
     /**
      * 取得該次交易中的股票名稱
      * @return 該次交易中的股票名稱
      */
-    public abstract String getStockName();
+    public String getStockName(){
+
+        //TODO
+    }
 
     /**
      * 取得該次交易的時間
      * @return 該次交易的時間
      */
-    public abstract Date getDateAndTime();
+    public Date getDateAndTime(){
+
+        //TODO
+    }
 
     /**
      * 取得該次交易的盈虧
      * @return 該次交易的盈虧
      */
-    public abstract double getProfitAndLoss();
+    public double getProfitAndLoss(){
+
+        //TODO
+    }
 
     /**
      * 取得該次交易的股數
      * @return 該次交易的股數
      */
-    public abstract int getStockCount();
+    public int getStockCount(){
+
+        //TODO
+    }
 
     /**
      * 儲存該次交易的股票名稱
      * @param stockName : 該次交易的股票名稱
      */
-    public abstract void setStockName(String stockName);
+    public void setStockName(String stockName){
+
+        //TODO
+    }
 
     /**
      * 儲存該次交易的時間
@@ -42,17 +57,26 @@ public interface Deal {
      * @param minute : 幾分
      * @param second : 幾秒
      */
-    public abstract void setDateAndTime(int year, int month, int date, int hour, int minute, int second);
+    public void setDateAndTime(int year, int month, int date, int hour, int minute, int second){
+
+        //TODO
+    }
 
     /**
      * 儲存該次交易的盈虧
      * @param profitAndLoss : 該次交易的盈虧
      */
-    public abstract void setProfitAndLoss(double profitAndLoss);
+    public void setProfitAndLoss(double profitAndLoss){
+
+        //TODO
+    }
 
     /**
      * 儲存該次交易的股數
      * @param stockCount : 該次交易的股數
      */
-    public abstract void setStockCount(int stockCount);
+    public void setStockCount(int stockCount){
+
+        //TODO
+    }
 }
